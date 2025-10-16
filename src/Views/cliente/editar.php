422797e2fd418fddd8071dafee43adff7d3bc5e3
Marielly <142667076+marymtts@users.noreply.github.com>
2025-10-16 20:29:39 -0300

    Delete editar.php

--- src/Views/cliente/editar.php
+++
@@ -1,8 +0,0 @@
-<?php require __DIR__ . '/../layout/header.php'; ?>
-
-<div class="d-flex justify-content-between align-items-center mb-4">
-    <h1>Edição de Cliente</h1>
-    <a class="btn btn-secondary" href="<?php echo BASE_URL; ?>/clientes">Voltar</a> 
-</div>
-
-<?php require __DIR__ . '/../layout/footer.php'; ?>
