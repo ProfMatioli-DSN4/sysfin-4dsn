5e249f830e785193b0d39a0953920e4c49507a76
EduardoRPortela <cl202184@g.unicamp.br>
2025-10-28 16:23:02 -0300

    Duplicação de URL corrigida no relatório de vendas

--- src/Views/relatorio-venda/venda_por_periodo.php
+++ src/Views/relatorio-venda/venda_por_periodo.php
@@ -2,8 +2,7 @@
 
 <div class="container mt-4">
     <h2>Relatório de Vendas por Período</h2>
-
-    <form method="POST" action="relatorio-venda/gerar" class="row g-3 mb-4">
+    <form method="POST" action="<?php echo BASE_URL; ?>/relatorio-venda/gerar" class="row g-3 mb-4">
         <div class="col-md-4">
             <label class="form-label">Data Inicial</label>
             <input type="date" name="data_inicial" class="form-control" required>
@@ -17,7 +16,7 @@
         </div>
     </form>
 
-    <?php if (!empty($vendas)): ?>
+    <?php if (isset($vendas)): ?>
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -39,11 +38,6 @@
 
         <h5 class="mt-3">Número total de vendas: <?= $totalVendas ?></h5>
         <h5>Valor total vendido: R$ <?= number_format($valorTotal, 2, ',', '.') ?></h5>
-    <?php elseif (isset($_POST['data_inicial'])): ?>
-        <div class="alert alert-warning mt-3">
-            Nenhuma venda encontrada no período selecionado.
-        </div>
     <?php endif; ?>
 </div>
-
 <?php require __DIR__ . '/../layout/footer.php'; ?>
